e5e187a67c0807ab98f8eedf0a385a1476e8f040
David Barkman <david.barkman13@gmail.com>
2022-12-15 19:04:48 -0700

    Initial Commit

---
+++ everyearthquake/everyearthquakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  everyearthquakeApp.swift
+//  everyearthquake
+//
+//  Created by David Barkman on 12/15/22.
+//
+
+import SwiftUI
+
+@main
+struct everyearthquakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
